e253f7a2c2fe009e71c6d1c15b015e6b9ffe4f5b
Wei He <hewei196126@gmail.com>
2018-11-30 20:33:27 -0500

    receiver

--- src/serverhelper/HttpfsServer.py
+++ src/serverhelper/HttpfsServer.py
@@ -2,57 +2,63 @@ import socket
 import threading
 from .RequestHandler import RequestHandler
 from .ResponseCreator import ResponseCreator
+from udp.PacketTypes import PacketTypes
+from udp.Packet import Packet
 from udp.UdpTransporter import UdpTransporter
 from udp.UdpReceiver import UdpReceiver
 import datetime
 
 REQUEST_TYPE = 0
+packet_types = PacketTypes()
+
 class HttpfsServer:
     def __init__(self, verbose, port, directory):
         self.verbose = verbose
         self.port = port
         self.directory = directory
+        self.clients = {}
     
     def start(self):
-        listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
-            listener.bind(('', self.port))
-            listener.listen(5)
+            conn.bind(('', self.port))
             if (self.verbose):
                 print('httpfs server is listening at ', self.port)
             while True:
-                conn, addr = listener.accept()
-                threading.Thread(target=self.handle_client, args=(conn, addr)).start()
-                #data, sender = listener.recvfrom(1024)
+                data, sender = conn.recvfrom(1024)
+                self.handle_client(conn, data, sender)
+                #TODO: Multi clients
                 #threading.Thread(target=self.handle_client, args=(listener, data, sender)).start()
         finally:
-            listener.close()
+            conn.close()
     
-    def handle_client(self, conn, addr):
-        
-        #receive request, use sliding window
-        request = conn.recv(1024).decode("utf-8")
-        udpReceiver = UdpReceiver()
-        udpReceiver.receive(request)
-        #------
-        if (self.verbose):
-            print(request)
-        request_array = request[0:request.index('\r\n\r\n')].split()
-        request_body = request[request.index('\r\n\r\n'):].replace('\r\n\r\n', '')
-        request_type = request_array[REQUEST_TYPE]
-        response = ''
-        try:
-            response = {}
-            if (request_type.lower() == 'get'):
-                response = RequestHandler(request_array, self.directory, request_body).process_get()
-            elif (request_type.lower() == 'post'):
-                response = RequestHandler(request_array, self.directory, request_body).process_post()
-        except:
-            response = {'status': 500, 'content': ''}
-        finally:
-            http_response = ResponseCreator.create_response(response['status'], response['content'], response['mimetype'])
+    def handle_client(self, conn, data, sender):
+        p = Packet.from_bytes(data)
+        peer ="%s:%s" % (p.peer_ip_addr, p.peer_port)
+        if (p.packet_type == packet_types.SYN):
+            udpTransporter = UdpTransporter(conn)
+            udpTransporter.handshake_receive(p, sender)
+            self.clients[peer] = udpTransporter
+
+        elif p.packet_type in [packet_types.DATA]:
+            udpReceiver = self.clients[peer]
+            request = udpReceiver.receive(p,sender)
             if (self.verbose):
-                print(http_response)
-            
-            udpTransporter = UdpTransporter()
-            udpTransporter.send(http_response)
+                print(request)
+            request_array = request[0:request.index('\r\n\r\n')].split()
+            request_body = request[request.index('\r\n\r\n'):].replace('\r\n\r\n', '')
+            request_type = request_array[REQUEST_TYPE]
+            response = ''
+            try:
+                response = {}
+                if (request_type.lower() == 'get'):
+                    response = RequestHandler(request_array, self.directory, request_body).process_get()
+                elif (request_type.lower() == 'post'):
+                    response = RequestHandler(request_array, self.directory, request_body).process_post()
+            except:
+                response = {'status': 500, 'content': ''}
+            finally:
+                http_response = ResponseCreator.create_response(response['status'], response['content'], response['mimetype'])
+                if (self.verbose):
+                    print(http_response)
+                udpTransporter.send(http_response)
