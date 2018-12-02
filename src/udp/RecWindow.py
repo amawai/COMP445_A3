015304c642216ed0b7f6c4ffebd3efb1c28f4f5d
Amanda <amawai@hotmail.com>
2018-12-02 04:10:22 -0500

    Add real receivewindow file, without testing and with relative imports

--- src/udp/RecWindow.py
+++ src/udp/RecWindow.py
@@ -1,5 +1,5 @@
-from Packet import Packet
-from PacketTypes import PacketTypes
+from .Packet import Packet
+from .PacketTypes import PacketTypes
 
 packet_types = PacketTypes()
 
@@ -44,22 +44,3 @@ class RecWindow:
     
     def valid_sequence_nums(self):
         return [num % self.max_seq_num for num in range(self.current_seq_start, self.current_seq_start + self.window_size)]
-
-p0 = Packet(packet_type=1,seq_num=0, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-p1 = Packet(packet_type=1,seq_num=1, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-p2 = Packet(packet_type=1,seq_num=2, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-p3 = Packet(packet_type=1,seq_num=3, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-p4 = Packet(packet_type=1,seq_num=4, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-p5 = Packet(packet_type=1,seq_num=5, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-p6 = Packet(packet_type=1,seq_num=6, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-p7 = Packet(packet_type=packet_types.FINAL_PACKET,seq_num=7, peer_ip_addr='localhost',peer_port=8007, payload="meh".encode())
-
-window = RecWindow(8)
-window.insert_packet(p1)
-window.insert_packet(p2)
-window.insert_packet(p3)
-window.insert_packet(p0)
-window.insert_packet(p6)
-window.insert_packet(p7)
-window.insert_packet(p4)
-window.insert_packet(p5)
